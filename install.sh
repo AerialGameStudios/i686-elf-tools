3a146c2977a7f8509ebc2496eafa248b32b4ecad
ozhanghe <93566273+ozhanghe@users.noreply.github.com>
2022-11-11 10:59:14 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/lordmilko/i686-elf-tools/releases/download/7.1.0/i686-elf-tools-linux.zip
+sudo unzip -d /usr i686-elf-tools-linux.zip
+rm i686-elf-tools-linux.zip
